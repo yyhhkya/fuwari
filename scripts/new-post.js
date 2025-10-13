5572ff86145a1061308db388ab67a5b13e37aaa9
Yy <i@hiwer.cn>
2025-10-14 07:49:03 +0800

    优化文章排序

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -8,8 +8,10 @@ function getDate() {
 	const year = today.getFullYear();
 	const month = String(today.getMonth() + 1).padStart(2, "0");
 	const day = String(today.getDate()).padStart(2, "0");
+	const hours = String(today.getHours()).padStart(2, "0");
+	const minutes = String(today.getMinutes()).padStart(2, "0");
 
-	return `${year}-${month}-${day}`;
+	return `${year}-${month}-${day}T${hours}:${minutes}:00`;
 }
 
 const args = process.argv.slice(2);
