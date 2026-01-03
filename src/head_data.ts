ed4230bc79860d612ab7303ce13eaee2d7bbcf7d
Yy <1163635725@qq.com>
2026-01-03 21:11:33 +0800

    添加Microsoft Clarity支持

--- src/head_data.ts
+++ src/head_data.ts
@@ -43,6 +43,14 @@ export const headData: HeadData = {
 
       gtag('config', 'G-HNTCZ055SX');
     </script>
+    <!-- Microsoft Clarity -->
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "uvoimo553n");
+    </script>
   `,
 
 	// 自定义 meta 标签示例
